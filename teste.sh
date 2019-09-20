873000433518cae6c9860f33afbf099d057f8c41
CaleODE <alexandre.alvesandrade@hotmail.com>
2019-09-20 10:59:23 -0300

    att teste.sh

--- teste.sh
+++ teste.sh
@@ -18,11 +18,24 @@ echo "A saido do ls -l foi: $saida_armazenada"
 echo "Diga-me, qual a sua idade?"
 read idade
 echo "Sua idade é: $idade"
-if [ $idade -gt 20 ]
+
+
+if [ $idade -gt 20 ] #Verifica seum valor é maior que outro
 then 
 	echo "velho"
+elif [ $idade -eq 10 ] #verifica igualdade de variaveis
+then
+	echo "muito novo"
 else
 	echo "novo" 
 fi
+
+
+
+
+
+
+
+
 
 echo "Shell terminado com sucesso!"
